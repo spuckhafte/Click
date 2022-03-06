34aa533ded31951cdc90b8c90c6fec417eac93bb
Rakshit <70335252+spuckhafte@users.noreply.github.com>
2022-03-06 20:22:44 +0530

    ::synced the project with JDb@v2:: => made changes, added comments

--- client/index.js
+++ client/index.js
@@ -1,4 +1,6 @@
 const socket = io.connect('http://localhost:6969')
+
+// html elements
 const logUsername = $('#login-name')
 const regUsername = $('#register-name')
 const logPassword = $('#login-pass')
@@ -6,98 +8,102 @@ const regPasssword = $('#register-pass')
 const logButton = $('.login-btn')
 const registerBtn = $('.sign-btn')
 const registerMail = $('#register-mail')
-
 const usermailInVerification = $('#verify-mail')
 const verifyOtp = $('#ver-submit-btn')
 const resendOtp = $('#resend-otp-btn-')
-
 const changeToLogBtn = $('#log-btn')
 const changeToRegBtn = $('#register-btn')
 
-logButton.click(event => {
+logButton.click(event => { // login
     event.preventDefault()
     if (logUsername.val().length <= 2 || logPassword.val().length < 8) {
         _showLoginRegisterError('Fill in all fields as indicated', 'login')
     } else {
         let username = logUsername.val()
         let password = logPassword.val()
-        socket.emit('login', { username, password })
+        socket.emit('login', { username, password }) // if everything is correct, send user to server
     }
 })
 
-registerBtn.click(event => {
+registerBtn.click(event => { // register
     event.preventDefault()
     if (regUsername.val().length <= 2 || regPasssword.val().length < 8) {
         _showLoginRegisterError('Fill in all fields as indicated', 'register')
     } else {
+        // css
         registerBtn.unbind('click')
         registerBtn.attr('disabled', true)
         registerBtn[0].innerText = 'Registering...'
         registerBtn.css('cursor', 'not-allowed')
         changeToLogBtn.css('display', 'none')
+
         let username = regUsername.val()
         let mail = registerMail.val()
         let password = regPasssword.val()
-        socket.emit('register', { username, password, mail })
+        socket.emit('register', { username, password, mail }) // if everything is correct, send user to server
     }
 })
 
-changeToLogBtn.click(event => {
+changeToLogBtn.click(event => { // change to login screen
     event.preventDefault()
     $('.login').css('display', 'block')
     $('.register').css('display', 'none')
 })
 
-changeToRegBtn.click(event => {
+changeToRegBtn.click(event => { // change to register screen
     event.preventDefault()
     $('.login').css('display', 'none')
     $('.register').css('display', 'block')
 })
 
-verifyOtp.click(event => {
+verifyOtp.click(event => { // verify otp btn
     event.preventDefault();
     let usermail = usermailInVerification[0].outerText
     let otp = $('#verify-code').val()
-    socket.emit('verify-otp', [usermail, otp])
+    socket.emit('verify-otp', [usermail, otp]) // send otp to server
 })
 
-socket.on('sign-log-error', (type, err) => {
+socket.on('sign-log-error', (type, err) => { // error in login/register
+    // show error based on type
     type === 'login' ? _showLoginRegisterError(err, 'login') : _showLoginRegisterError(err, 'register')
 })
 
-socket.on('sign-log-success', (type, user) => {
-    if (type === 'login') console.log(user)
-    else alert('Registered successful, kindly login')
+socket.on('sign-log-success', (type, user) => { // success in login/register
+    if (type === 'login') {
+        $('#login-error').css('display', 'none')
+        console.log(user)
+        alert('Login Successful')
+        // need to work on this
+    }
+    else {
+        $('#register-error').css('display', 'none')
+        alert('Registered successful, kindly login')
+        location.reload(); // reload page in order to show login screen
+    }
 })
 
-socket.on('otp-sent-verify', mail => {
+socket.on('otp-sent-verify', mail => { // otp sent to user, show verify otp screen
     usermailInVerification.text(mail)
     $('.register').css('display', 'none')
     $('.verify').css('display', 'block')
     setTimeout(() => {
-        if ($('.verify').css('display') === 'block') {
-            socket.emit('otp-timeout', mail)
-            $('.verify').css('display', 'none')
-            $('.register').css('display', 'block')
-            $('#register-error').text(err)
-            $('#register-error').css('display', 'block')
-        }
+        // if user does not verify in 5 mins, back to register screen
+        if ($('.verify').css('display') === 'block') socket.emit('otp-timeout', mail)
     }, 300000)
 })
 
-socket.on('otp-err', (type, err) => {
+socket.on('otp-err', (type, err) => { // wrong otp (server's call)
     if (type === 'exp') {
+        $('#register-error').text(err);
+        $('#register-error').css('display', 'block');
         $('.verify').css('display', 'none')
         $('.register').css('display', 'block')
-        _showLoginRegisterError(err, 'register')
     }
-    if (type === 'inv') {
-        $('#verify-error').text(err)
-    }
+    if (type === 'inv') $('#verify-error').text(err)
 })
 
 
-const _showLoginRegisterError = (err, type) => {
+const _showLoginRegisterError = (err, type) => { // show errors in login/register
     if (type === 'login') {
         $('#login-error').text(err)
         $('#login-error').css('display', 'block')
--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-const jdb = require('../packages/Jdb/jdb.js');
+const jdb = require('../packages/Jdb@v2/jdb');
 const validate = require('../packages/mValid/validate.js');
 const http = require('http').createServer()
 const fs = require('fs')
@@ -6,96 +6,98 @@ const io = require('socket.io')(http, {
     cors: { origin: "*" }
 });
 const sha256 = require('js-sha256');
-const { assert } = require('console');
-//db format -> "username": ["password", "email", ...]
 
-const unverifiedUsers = {}; // { mail: username, pass, otp, socket.id }
+//db format -> 1. username 1. [email, password] 1. etc
+
+const unverifiedUsers = {}; // { mail: username, pass, otp, socket.id } -> info of users to be verified
 
 io.on('connection', socket => {
-    socket.on('login', data => { // when users logs in
+    socket.on('login', data => { // user logs in
         let username = data.username
         let password = data.password
-        jdb.assignI('data', 'users', { 'test': '123' })
-        let allUsers = Object.keys(jdb.__getIEl('data', 'users')) // get all users from iElement
-        if (allUsers.includes(username)) {
-            let user = jdb.__getIEl('data', 'users')[username]
-            user.push(username)
-            let checkPass = sha256(password)
-            if (checkPass === user[0]) {
-                socket.emit('sign-log-success', 'login', user)
-            } else {
-                socket.emit('sign-log-error', 'login', 'No such user or wrong password')
-            }
-        } else {
-            socket.emit('sign-log-error', 'login', 'No such user or wrong password')
-        }
+        let allUsers = Object.values(jdb.getEl('data', 'user')) // get all the usernames
+
+        if (allUsers.includes(username)) { // if username exists
+            let user = JSON.parse(jdb.getR('data', 'moral', ['user', username])['userinfo']); // get user info [email, password]
+            user.push(username) // add username to userinfo
+
+            let checkPass = sha256(password) // password entered by user
+            if (checkPass === user[1]) socket.emit('sign-log-success', 'login', user) // if password is correct
+            else socket.emit('sign-log-error', 'login', 'No such user or wrong password')
+
+        } else socket.emit('sign-log-error', 'login', 'No such user or wrong password')
     })
 
     socket.on('register', data => { // when users registers
         let username = data.username
         let password = sha256(data.password)
         let mail = data.mail
-        let allUsers = Object.keys(jdb.__getIEl('data', 'users')) // get all users from iElement
-        if (allUsers.includes(username)) { // check if username is already taken
-            socket.emit('sign-log-error', 'register', 'Username already registered')
-        } else {
-            let allMails = []
-            Object.keys(jdb.__getIEl('data', 'users')).forEach(user => {
-                user !== "0" ? allMails.push(jdb.__getIEl('data', 'users')[user][1]) : null
-            })
-            if (allMails.includes(mail)) { // check if mail is already registered
-                socket.emit('sign-log-error', 'register', 'Mail already registered')
-            } else {
+
+        let allUsernames = Object.values(jdb.getEl('data', 'user')) // get all the usernames
+        let allMails = Object.values(jdb.getEl('data', 'userinfo')) // get mails of all the users [email, password]
+
+        allMails.shift(); allUsernames.shift(); // remove first element of both arrays (element id)
+        allMails.forEach((detail, index) => { // extract mail from the data
+            allMails[index] = JSON.parse(detail)[0]
+        })
+
+        // check if username is already registered
+        if (allUsernames.includes(username)) socket.emit('sign-log-error', 'register', 'Username already registered')
+        else {
+            // check if mail is already registered
+            if (allMails.includes(mail)) socket.emit('sign-log-error', 'register', 'Mail already registered')
+            else {
+                // check the format of the mail
                 const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
                 let mailValid = re.test(String(mail).toLowerCase())
+
                 if (mailValid) {
+                    // create object to send mail (required by validate.sendMail)
                     let mailObj = {
                         'mail': mail,
                         'username': username,
                         'password': password,
                         'socket': socket
                     }
-                    if (Object.keys(unverifiedUsers).includes(mail)) {
-                        socket.emit('sign-log-error', 'register', 'Mail being registered')
-                    } else _sendOtp(mailObj, 'register')
-                }
-                else socket.emit('sign-log-error', 'register', 'Invalid mail')
 
-                // now if user is verified, then add user to db
+                    if (Object.keys(unverifiedUsers).includes(mail)) socket.emit('sign-log-error', 'register', 'Mail being registered') // mail is being registered, err
+                    else _sendOtp(mailObj, 'register') // send otp to mail
+
+                } else socket.emit('sign-log-error', 'register', 'Invalid mail')
             }
         }
     });
 
-    socket.on('otp-timeout', mail => {
+    socket.on('otp-timeout', mail => { // otp timeout
+        // delete the user from unverifiedUsers
         Object.keys(unverifiedUsers).includes(mail) ? delete unverifiedUsers[mail] : null
     })
 
-    socket.on('verify-otp', data => {
+    socket.on('verify-otp', async data => { // user verifies otp
         let mail = data[0]
         let otp = data[1]
-        if (Object.keys(unverifiedUsers).includes(mail)) {
-            if (otp === unverifiedUsers[mail][2]) {
+        if (Object.keys(unverifiedUsers).includes(mail)) { // if mail is being registered, true
+            if (otp === unverifiedUsers[mail][2]) { // if otp is correct
                 let user = unverifiedUsers[mail][0]
                 let password = unverifiedUsers[mail][1]
+                let email = mail
 
-                let userObj = {}
-                userObj[user] = [password, mail]
-                socket.emit('sign-log-success', 'register', userObj)
-                jdb.assignI('data', 'users', userObj)
-
+                let moralObject = {
+                    'user': user,
+                    'userinfo': [email, password]
+                }
+                socket.emit('sign-log-success', 'register')
+                await jdb.assignR('data', moralObject) // assign user to database if verified
                 delete unverifiedUsers[mail]
-            } else {
-                socket.emit('otp-err', 'inv', 'Invalid otp')
-            }
-        } else {
-            socket.emit('otp-err', 'exp', 'Otp expired')
-        };
+
+            } else socket.emit('otp-err', 'inv', 'Invalid otp')
+        } else socket.emit('otp-err', 'exp', 'Otp expired')
     });
 
-    socket.on('disconnect', () => {
+    socket.on('disconnect', () => { // user disconnects
         Object.keys(unverifiedUsers).forEach(mail => {
             if (unverifiedUsers[mail][3] === socket.id) {
-                delete unverifiedUsers[mail]
+                delete unverifiedUsers[mail] // remove the user from unverifiedUsers, if there
             }
         })
     })
@@ -103,7 +105,7 @@ io.on('connection', socket => {
 
 
 
-function _sendOtp(data, type) {
+function _sendOtp(data, type) { // send otp to mail
     if (type === 'register') {
         let mail = data['mail']
         let username = data['username']
@@ -112,8 +114,8 @@ function _sendOtp(data, type) {
         validate.sendMail(mail, otpData => {
             if (!otpData[0]) socket.emit('sign-log-error', 'register', 'Invalid mail')
             else {
-                unverifiedUsers[mail] = [username, password, otpData[1], socket.id]
-                socket.emit('otp-sent-verify', mail)
+                unverifiedUsers[mail] = [username, password, otpData[1], socket.id] // add user to unverifiedUsers
+                socket.emit('otp-sent-verify', mail) // notify the client, otp sent!
             }
         })
     }
